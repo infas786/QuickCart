6add593547a7d881743e53f46adb42ffcb99def9
infas786 <106691143+infas786@users.noreply.github.com>
2025-06-01 00:06:31 +0530

    Fix Inngest arrow function syntax

--- config/inngest.js
+++ config/inngest.js
@@ -9,45 +9,45 @@ export const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    const {id, first_name, last_name, email_addresses, image_url} = event.data
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
     const userData = {
-      _id:id,
+      _id: id,
       email: email_addresses[0].email_address,
       name: first_name + ` ` + last_name,
-      imageUrl: image_url
-    }
-    await connectDB()
-    await User.create(userData)
+      imageUrl: image_url,
+    };
+    await connectDB();
+    await User.create(userData);
   }
 );
 
 export const syncUserUpdate = inngest.createFunction(
   {
-    id: 'update-user-from-clerk'
+    id: "update-user-from-clerk",
   },
-  { event: 'clerk/user.updated' },
-  await ({event}) => {
-    const {id, first_name, last_name, email_addresses, image_url} = event.data
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
     const userData = {
-      _id:id,
+      _id: id,
       email: email_addresses[0].email_address,
       name: first_name + ` ` + last_name,
-      imageUrl: image_url
-    }
-    await connectDB()
-    await User.findByIdAndUpdate(id,userData)
+      imageUrl: image_url,
+    };
+    await connectDB();
+    await User.findByIdAndUpdate(id, userData);
   }
-)
+);
 
 export const syncUserDeletion = inngest.createFunction(
   {
-    id: 'delete-user-with-clerk'
+    id: "delete-user-with-clerk",
   },
-  {event: 'clerk/user.deleted'},
-  await ({event}) => {
-    const {id} = event.data
-    
-    await connectDB()
-    await User.findByIdAndDelete(id)
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+
+    await connectDB();
+    await User.findByIdAndDelete(id);
   }
-)
+);
